bfc9112006cbc50fffe7b5375eab43aee4c44622
Lloyd Park <llpark30@gmail.com>
2017-07-05 12:56:02 -0400

    fixed browseraction badge bug

--- contentScript.js
+++ contentScript.js
@@ -8,6 +8,7 @@ function playnext(){
 
   chrome.storage.sync.get('storagequeue', function(data){
     var nextvideo = data.storagequeue.shift();
+    chrome.browserAction.setBadgeText({text: '' + data.storagequeue.length});
     chrome.storage.sync.set(data);
     window.location.href = nextvideo;
   })
